0e18a1dc95e27dc6911c6935590f721f73db24eb
serene1234 <S10268407@connect.np.edu.sg>
2025-07-13 02:05:42 +0800

    feat: middleware functions for facilities, bookmarks and reviews

---
+++ middlewares/facilitiesValidation.js
@@ -0,0 +1,81 @@
+const Joi = require("joi");
+
+const coordinateSchema = Joi.number().required().messages({
+    "number.base": "Latitude and Longitude must be numbers",
+    "any.required": "Latitude and Longitude are required",
+})
+
+const radiusSchema = Joi.number().positive().optional().messages({
+    "number.base": "Radius must be a number",
+    "number.positive": "Radius must be a positive number",
+})
+
+const facilityIdSchema = Joi.object({
+    id: Joi.number().integer().positive().required().messages({
+        "number.base": "Facility ID must be a number",
+        "number.integer": "Facility ID must be an integer",
+        "number.positive": "Facility ID must be a positive number",
+        "any.required": "Facility ID is required",
+    }),
+});
+
+const facilityTypeSchema = Joi.string().required().messages({
+    "string.base": "Facility type must be a string",
+    "any.required": "Facility type is required",
+});
+
+function validateLocationAccess(req, res, next) {
+    const schema = Joi.object({
+        lat: coordinateSchema,
+        lng: coordinateSchema,
+    });
+
+    const { error } = schema.validate(req.query, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map((d) => d.message).join(", ");
+        return res.status(400).json({ error: errorMessages });
+    }
+    next();
+}
+
+function validateNearbyFacilities(req, res, next) {
+    const schema = Joi.object({
+        lat: coordinateSchema,
+        lng: coordinateSchema,
+        rad: radiusSchema,
+    });
+
+    const { error } = schema.validate(req.query, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map((d) => d.message).join(", ");
+        return res.status(400).json({ error: errorMessages });
+    }
+    next();
+}
+
+function validateFacilityId(req, res, next) {
+    const params = { id: Number(req.params.id) };
+    const { error } = facilityIdSchema.validate(params, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map((d) => d.message).join(", ");
+        return res.status(400).json({ error: errorMessages });
+    }
+    next();
+}
+
+function validateFacilityType(req, res, next) {
+    const params = { type: req.params.type };
+    const { error } = facilityTypeSchema.validate(params, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map((d) => d.message).join(", ");
+        return res.status(400).json({ error: errorMessages });
+    }
+    next();
+}
+
+module.exports = {
+    validateLocationAccess,
+    validateNearbyFacilities,
+    validateFacilityId,
+    validateFacilityType,
+};
