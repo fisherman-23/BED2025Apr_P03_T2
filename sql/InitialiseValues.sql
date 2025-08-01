ee53e0a700fd7c063c82998a99c983354b4f9c2b
Jing Shun <ooijs234@gmail.com>
2025-08-01 14:05:17 +0800

    feat: removed conversation and message data in InitialiseValues

--- sql/InitialiseValues.sql
+++ sql/InitialiseValues.sql
@@ -1,15 +1,3 @@
-INSERT INTO Conversations (User1ID, User2ID)
-VALUES
-(1, 2),  -- Alice & Bob
-(1, 3);  -- Alice & Carol
-
-INSERT INTO Messages (ConversationID, SenderID, Content)
-VALUES
-(1, 1, 'Hey Bob, how are you?'),          -- Alice to Bob
-(1, 2, 'Hi Alice! I am good, thanks!'),   -- Bob replies
-(2, 3, 'Hey Alice, ready for the meeting?'), -- Carol to Alice
-(2, 1, 'Yes, Carol! Starting soon.');     -- Alice replies
-
 SET IDENTITY_INSERT Users ON;
 
 INSERT INTO Users (
